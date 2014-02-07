1697279e6037f901e5917f03b94bfb4f4a033e74
Riccardo Tacconi <Riccardo.Tacconi@wonga.com>
2014-02-07 18:03:26 +0000

    Files from qm

---
+++ spec/pantry_ec2_resize_command_handler/pantry_ec2_resize_command_handler_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require_relative '../../pantry_ec2_resize_command_handler/pantry_ec2_resize_command_handler'
+
+describe Wonga::Daemon::PantryEc2ResizeCommandHandler do
+  it_behaves_like 'handler'
+  describe "#handle_message" do
+    #add in unit test code for handle message here.
+  end
+end
+
