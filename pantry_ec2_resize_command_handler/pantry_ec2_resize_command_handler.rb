b5f5c140200cb21a9dbab6ebaefc05f19c20dcc1
Aleksey Dragan <aleksey.dragan@wonga.com>
2014-11-20 17:09:46 +0200

    TD-1536 Cleanup a terminated instance from daemon
    Change-Id: I539ab1468d68807c8d7931cf11a5d71f90b195a3

--- pantry_ec2_resize_command_handler/pantry_ec2_resize_command_handler.rb
+++ pantry_ec2_resize_command_handler/pantry_ec2_resize_command_handler.rb
@@ -1,8 +1,9 @@
 module Wonga
   module Daemon
     class PantryEc2ResizeCommandHandler
-      def initialize(publisher, logger, aws_resource)
+      def initialize(publisher, error_publisher, logger, aws_resource)
         @publisher = publisher
+        @error_publisher = error_publisher
         @logger = logger
         @aws_resource = aws_resource
       end
@@ -10,11 +11,20 @@ module Wonga
       def handle_message(message)
         @logger.info message.inspect
         return unless @aws_resource.stop(message)
-        @logger.info "Instance stopped"
+        @logger.info 'Instance stopped'
         ec2 = @aws_resource.find_server_by_id(message['instance_id'])
-        ec2.instance_type = message["flavor"]
-        @logger.info "Changed flavor and going to publish the event"
+        if ec2.status == :terminated
+          send_error_message(message)
+          return
+        end
+        ec2.instance_type = message['flavor']
+        @logger.info 'Changed flavor and going to publish the event'
         @publisher.publish(message)
+      end
+
+      def send_error_message(message)
+        @logger.info 'Send request to cleanup an instance'
+        @error_publisher.publish(message)
       end
     end
   end
--- spec/pantry_ec2_resize_command_handler/pantry_ec2_resize_command_handler_spec.rb
+++ spec/pantry_ec2_resize_command_handler/pantry_ec2_resize_command_handler_spec.rb
@@ -3,39 +3,58 @@ require_relative '../../pantry_ec2_resize_command_handler/pantry_ec2_resize_comm
 
 describe Wonga::Daemon::PantryEc2ResizeCommandHandler do
   let(:logger) { instance_double('Logger').as_null_object }
-  let(:publisher) { instance_double('Wonga::Daemon::Publisher') }
-  let(:message) {
-    {"flavor" =>  "m1.medium", "id" => 1, "instance_id" => "i-ebf1a1a5"}
-  }
-  let(:aws_resource) { instance_double('Wonga::Daemon::AWSResource.new') }
-  subject { Wonga::Daemon::PantryEc2ResizeCommandHandler.new(publisher, logger, aws_resource) }
+  let(:publisher) { instance_double('Wonga::Daemon::Publisher', publish: true) }
+  let(:error_publisher) { instance_double('Wonga::Daemon::Publisher', publish: true) }
+  let(:message) do
+    { 'flavor' =>  'm1.medium', 'id' => 1, 'instance_id' => 'i-ebf1a1a5' }
+  end
+  let(:aws_resource) { instance_double('Wonga::Daemon::AWSResource') }
+  subject { Wonga::Daemon::PantryEc2ResizeCommandHandler.new(publisher, error_publisher, logger, aws_resource) }
   it_behaves_like 'handler'
 
-  describe "#handle_message" do
-    it "exits when the instance is terminated or not found" do
+  describe '#handle_message' do
+    it 'exits when the instance is terminated or not found' do
       expect(aws_resource).to receive(:stop).with(message).and_return(nil)
       subject.handle_message(message)
     end
 
-    describe "when the instance is stopped" do
+    describe 'when the instance is stopped' do
       before(:each) do
         allow(aws_resource).to receive(:stop).with(message).and_return(true)
-        @instance = instance_double('AWS::EC2::Instance')
+        @instance = instance_double('AWS::EC2::Instance', status: true)
         allow(aws_resource).to receive(:find_server_by_id).and_return(@instance)
       end
 
-      it "changes the instance_type " do
-        expect(@instance).to receive(:instance_type=).with(message["flavor"])
+      it 'changes the instance_type' do
+        expect(@instance).to receive(:instance_type=).with(message['flavor'])
         allow(publisher).to receive(:publish)
         subject.handle_message(message)
       end
 
-      it "publishes the message" do
-        allow(@instance).to receive(:instance_type=).with(message["flavor"])
+      it 'publishes the message' do
+        allow(@instance).to receive(:instance_type=).with(message['flavor'])
         expect(publisher).to receive(:publish)
         subject.handle_message(message)
       end
     end
   end
-end
 
+  describe '#handle_message publishes message to error topic for terminated instance' do
+    let(:instance) { instance_double('AWS::EC2::Instance', status: :terminated).as_null_object }
+    let(:aws_resource) { instance_double('Wonga::Daemon::AWSResource', stop: true, find_server_by_id: 'i-ebf1a1a5', status: 'i-ebf1a1a5') }
+
+    before(:each) do
+      allow(aws_resource).to receive(:find_server_by_id).and_return(instance)
+    end
+
+    it 'publishes message to error topic' do
+      subject.handle_message(message)
+      expect(error_publisher).to have_received(:publish).with(message)
+    end
+
+    it 'does not publish message to topic' do
+      subject.handle_message(message)
+      expect(publisher).to_not have_received(:publish)
+    end
+  end
+end
